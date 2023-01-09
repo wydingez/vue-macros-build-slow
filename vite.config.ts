f3ff18c1a9c75ca42ea7ed63ee5d1abb1d642a2d
Lucuai <981342796@qq.com>
2023-01-09 16:42:21 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import timeReporter from 'vite-plugin-time-reporter';
+import VueMacros from "unplugin-vue-macros/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // VueMacros({}), 
+    timeReporter()
+  ],
+})
